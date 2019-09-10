ce30f2cbca24b03b554a4be220552743964faae7
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-10 19:52:35 -0400

    Validacoes do formulario

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Email inválido!')
+    .required('Email obrigatório!'),
+  password: Yup.string().required('Senha obrigatória!'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,7 +20,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="Gobarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="email" type="email" placeholder="Seu email" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome obrigatório!'),
+  email: Yup.string()
+    .email('Email inválido!')
+    .required('Email obrigatório!'),
+  password: Yup.string()
+    .min(6, 'É necessário mais de 6 caracteres!')
+    .required('Senha obrigatória!'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +23,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="Gobarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="name" type="text" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <Input name="password" type="password" placeholder="Sua senha" />
