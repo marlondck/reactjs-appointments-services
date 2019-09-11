403163dfc23a39442c42ae7e8350f248b48c0ea6
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-11 16:59:00 -0400

    Cadastro

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -30,4 +30,28 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, '/users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('Conta criada com sucesso!');
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha ao cadastrar!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
