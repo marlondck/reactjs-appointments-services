f9613f046a9d75608812d2d6a865b3e82baf9496
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-10 20:11:41 -0400

    Configurando o Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import store from './store';
+
 import Routes from './routes';
 import history from './services/history';
 import GlogalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlogalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlogalStyle />
+      </Router>
+    </Provider>
   );
 }
 
