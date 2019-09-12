77e121c780a71c80b249c04491faeaba28334b47
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-12 18:25:35 -0400

    Estilizando Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
-import api from '~/services/api';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('/appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>12 de Setembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Marlon Authority</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Fulano</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Ciclano</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
